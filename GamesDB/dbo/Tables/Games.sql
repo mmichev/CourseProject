8d67bad0240089c4843400bf2efc598a4c336d73
Mariyan Michev <Diegomsn107@gmail.com>
2018-06-06 16:16:23 +0300

    Add database

---
+++ GamesDB/dbo/Tables/Games.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Games] (
+    [ID]               INT            IDENTITY (1, 1) NOT NULL,
+    [Name]             NVARCHAR (200) NOT NULL,
+    [DateOfPublishing] DATE           NOT NULL,
+    [Category]         INT            NOT NULL,
+    CONSTRAINT [PK_Games] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_Games_Categories] FOREIGN KEY ([Category]) REFERENCES [dbo].[Categories] ([ID])
+);
+
